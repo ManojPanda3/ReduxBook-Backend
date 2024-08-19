4b1381f952c9df90fbefd105dd80d09e0a06c39b
Manoj <manojpandawork@gmail.com>
2024-08-19 10:21:39 +0530

    ready for production

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
-import app from "./app.js";
-import connectDB from "./db/index.db.js";
+import app from "../src/app.js";
+import connectDB from "../src/db/index.db.js";
 
 dotenv.config({ path: "./.env" });
 
