7753686311d2eb86d0785cdcb4adc8301f1244ca
Manoj <manojpandawork@gmail.com>
2024-08-19 16:53:51 +0530

    fix some error + add cors  whitelist

--- src/app.js
+++ src/app.js
@@ -1,27 +1,37 @@
 import express from "express";
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 // CORS setup so only allowed ip can call the api
-const options = {
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}
-app.use(cors(options));
+const whitelist = process.env.CORS_ORIGIN.split(",");
+console.log(whitelist);
+const corsOptions = {
+  origin: (ori, callback) => {
+    console.log(ori)
+    if (whitelist.indexOf("*") != -1) { callback(null, true) }
+    else if (whitelist.indexOf(ori) != -1) { callback(null, true) }
+    else { callback(new Error(`This address(${ori}) is not in whitelist :(`)); }
+  },
+  credentials: true,
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
+
 // some express configurations 
 app.use(express.json({ limit: "8kb" }));
 app.use(express.urlencoded({ extended: true, limit: "8kb" }));
 app.use(express.static("public")); // setup the public folder for all the static datas like images
+
+
 // setup cookieParser middleware
 app.use(cookieParser());
 
-
 // routes 
-import userRoutes from "./routes/user.route.js"
-import emailRoutes from "./routes/email.route.js"
-
+import userRoutes from "./routes/user.route.js";
+import emailRoutes from "./routes/email.route.js";
 
 app.get('/api', (req, res) => { return res.send("<p>/api/v1/{user,email}/<functionality>") });
 app.get('/', (req, res) => { return res.send("Hello world") });
